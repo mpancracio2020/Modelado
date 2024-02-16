0a7316853eed844cd422a26b747791ff25ff9144
mpancracio2020 <m.pancracio.2020@alumnos.urjc.es>
2024-02-16 10:35:27 +0100

    escenario v1

--- src/p1/main.py
+++ src/p1/main.py
@@ -2,36 +2,39 @@ import pybullet as p
 import pybullet_data
 import argparse
 import time
+import math
 
 
-parser = argparse.ArgumentParser(description="URDF viewer example")
-parser.add_argument("--urdf", type=str, required=True, help="/ejercicio.urdf")
-args = parser.parse_args()
-urdf_path = args.urdf
 
 physicsClient = p.connect(p.GUI) #or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,-9.8)
 
+robotOrientation = [0,0,math.pi/2] # set orientation husky.
+robotOrientationQ = p.getQuaternionFromEuler(robotOrientation)
+
 planeId = p.loadURDF("plane_transparent.urdf")
 
-startPos = [0,0,1]
+#robotId = p.loadURDF("husky/husky.urdf",[0, 0, 0], robotOrientationQ)
+startPos = [0,0,0.1]
 startOrientation = p.getQuaternionFromEuler([0,0,-3.15])
 
-robotId = p.loadURDF(urdf_path,startPos, startOrientation)
+barrierId = p.loadURDF("barrier.urdf",[0,10,0.1], startOrientation)
+rampaId = p.loadURDF("rampa.urdf",startPos, startOrientation)
 frictionId = p.addUserDebugParameter("jointFriction", 0 ,40, 10)
 torqueId = p.addUserDebugParameter("torque joint", 0 ,40, 5)
+
 p.setRealTimeSimulation(1)
 
 
-#p.setRealTimeSimulation(1)
+p.setRealTimeSimulation(1)
 
 try:
     while True:
         frictionForce = p.readUserDebugParameter(frictionId)
         jointTorque = p.readUserDebugParameter(torqueId)
-        p.setJointMotorControl2(robotId,1,p.VELOCITY_CONTROL, targetVelocity=0, force=frictionForce)
-        p.setJointMotorControl2(robotId,1,p.TORQUE_CONTROL, force=jointTorque)
+        # p.setJointMotorControl2(barrierId,0,p.VELOCITY_CONTROL, targetVelocity=0, force=frictionForce)
+        # p.setJointMotorControl2(barrierId,0,p.TORQUE_CONTROL, force=jointTorque)
         p.stepSimulation()
         time.sleep(1./24.)
             
